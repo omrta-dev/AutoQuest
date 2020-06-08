4adf5a4c70efd8c21d599eb0849d76288fb025cd
Omar Martinez <omar.martinez.sw@gmail.com>
2020-06-07 18:16:27 -0700

    Added doxygen documentation for AnimationFrame

--- game/components/Animation.hpp
+++ game/components/Animation.hpp
@@ -6,16 +6,20 @@
 
 namespace aik::component
 {
+    /**!
+     * @brief Contains the duration and index of each frame
+     */
     struct AnimationFrame
     {
-        float duration;
-        int animationIndex;
+        float duration; //< duration in seconds that this frame will be shown for
+        int animationIndex; //< the index of the animation in the sprite sheet
     };
 
+    //! @brief An animation is just an array of AnimationFrames with the index of the current frame
     class Animation
     {
     public:
-        std::vector<AnimationFrame> animationFrames;
-        unsigned int currentAnimationIndex;
+        std::vector<AnimationFrame> animationFrames; //< vector of frames for this animation
+        unsigned int currentAnimationIndex; //< contains the current index of the animation
     };
 }
